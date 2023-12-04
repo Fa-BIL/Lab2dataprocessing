f82a36406f6c71ecd286f08105e92b1cd679ec89
Savenkov Alexander <Fa.BIL@yandex.ru>
2023-12-04 16:07:07 +0400

    def get_real_path

--- goal_1.py
+++ goal_1.py
@@ -1,6 +1,11 @@
 import csv
 import os
 
+def get_real_path(abs_path: str, file_path: str)->str:
+    mut_path = os.path.join(os.path.commonpath([abs_path, file_path]), "")
+    real_path = abs_path.replace(mut_path, "")
+    return real_path
+
 def main(annotation_name: str, dataset_folder: str) -> None:
 
 
