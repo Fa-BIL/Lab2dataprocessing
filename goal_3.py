a141b330bb88abf68cac13e7616f0ce617a0d58e
Savenkov Alexander <Fa.BIL@yandex.ru>
2023-12-10 11:41:14 +0400

    goal_3

---
+++ goal_3.py
@@ -0,0 +1,8 @@
+import csv
+import os
+import shutil
+from random import sample
+
+
+if __name__ == "__main__":
+    main("C:\\Users\\MSI\\Desktop\\Programming\\Lab1webscraping\\dataset", "C:\\Users\\MSI\\Desktop\\Programming\\Lab2dataprocessing\\dataset_copy_3","annotations_3.csv")
