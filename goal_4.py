0a2559175fbba6db3e88d78793435850ad112af8
Savenkov Alexander <Fa.BIL@yandex.ru>
2023-12-10 13:54:46 +0400

    final goal_4

--- goal_4.py
+++ goal_4.py
@@ -1,1 +1,12 @@
 import os
+
+def get_next(class_mark: str, dataset_dir: str, num: int) -> str:
+
+    #Функция возвращает следующий абсолютный путь в 'dataset_dir' с классом 'class_mark'.
+
+    for subfolder in os.listdir(dataset_dir):
+        if subfolder == class_mark:
+            abs_subfolder = os.path.abspath(os.path.join(dataset_dir, subfolder))
+            files = os.listdir(abs_subfolder)
+            return os.path.join(abs_subfolder, files[num])
+    return None
