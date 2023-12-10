3f4e9f31cfd003044649db39667bcdcd7687c74c
Savenkov Alexander <Fa.BIL@yandex.ru>
2023-12-10 11:15:02 +0400

    final_1

--- goal_1.py
+++ goal_1.py
@@ -7,8 +7,18 @@ def get_real_path(abs_path: str, file_path: str)->str:
     return real_path
 
 def main(annotation_name: str, dataset_folder: str) -> None:
+    path_to_file = os.path.dirname(__file__)
+    with open(annotation_name, "w", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        for subfolder in os.listdir(dataset_folder):
+            folder = os.path.join(dataset_folder, subfolder)
+            for filename in os.listdir(folder):
+                abs_path = os.path.join(folder, filename)
+                rel_path = get_real_path(abs_path, path_to_file)
+                row = [abs_path, rel_path, subfolder]
+                writer.writerow(row)
 
 
 
 if __name__ == "__main__":
-    main()
+    main("annotations_1.csv", "C:\\Users\\MSI\\Desktop\\Programming\\Lab1webscraping\\dataset")
