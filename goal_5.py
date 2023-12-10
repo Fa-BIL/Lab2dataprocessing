a26380d94d3ca51adba8c42eb7bf5dffde05511c
Savenkov Alexander <Fa.BIL@yandex.ru>
2023-12-10 17:14:58 +0400

    goal_5 final

---
+++ goal_5.py
@@ -0,0 +1,27 @@
+import os
+
+from goal_4 import get_next
+
+
+class Iterator:
+    def __init__(self, dataset_dir: str, class_mark: str):
+        self.limit = 0
+        for subfolder in os.listdir(dataset_dir):
+            self.limit += len(os.listdir(os.path.join(dataset_dir, subfolder)))
+        self.counter = 0
+        self.class_mark = class_mark
+        self.dataset_dir = dataset_dir
+
+    def __next__(self):
+        if self.counter < self.limit:
+            self.counter += 1
+            return get_next(self.class_mark, self.dataset_dir, self.counter - 1)
+        else:
+            raise StopIteration
+
+
+a = Iterator(
+    os.path.join("c:\\", "Users", "MSI", "Desktop","Programming", "Lab1webscraping", "dataset"), "good"
+)
+for i in range(1000):
+    print(next(a))
